1618af8b9e1b164a563e589ae653660b3549069b
mxr123 <1583525155@qq.com>
2019-04-23 18:28:24 +0800

    添加项目文件。

---
+++ Mirror_Beatmap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mirror_Beatmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
