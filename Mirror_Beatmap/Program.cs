e22146e5701758af5091f347b40d59763d03c40a
mxr123 <1583525155@qq.com>
2019-04-29 16:29:32 +0800

    支持分块获取(range), 提高代码可读性

--- Mirror_Beatmap/Program.cs
+++ Mirror_Beatmap/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Transport;
 
 namespace Mirror_Beatmap
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Nancy.Responses.PartialFileResponse.SafePaths.Add("cache");
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                //.UseLibuv()
+                .UseKestrel(options =>
+                {
+                    //options.ListenUnixSocket("/dev/shm/asp.netcore.sock");
+                    options.ListenLocalhost(9999, configure =>
+                    {
+                        //configure.UseHttps("dev.zhzi233.cn.pfx", File.ReadAllText("keystorePass.txt"));
+                        configure.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+                    });
+                })
+            ;
     }
 }
