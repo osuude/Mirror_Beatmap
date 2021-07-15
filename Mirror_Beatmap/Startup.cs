d1f9225936c22e6d9044cabb0a2691b487cecb05
梦想远大 <1583525155@qq.com>
2021-07-15 11:18:22 +0800

    add publish settings, add proxy_pass support

--- Mirror_Beatmap/Startup.cs
+++ Mirror_Beatmap/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using CSRedis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
@@ -28,10 +30,14 @@ namespace MirrorBeatmap
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Loopback);
+            });
+
             var rc = new CSRedisClient(Configuration.GetConnectionString("Redis"));
             RedisHelper.Initialization(rc);
             services.AddSingleton<IDistributedCache>(new CSRedisCache(RedisHelper.Instance));
-
 
             services.AddControllers();
         }
@@ -39,10 +45,17 @@ namespace MirrorBeatmap
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             app.UseHttpsRedirection();
 
